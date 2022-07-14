bbcc3f1a87e4dd42eae5e20c5a011cafb3f06139
Arbaaz Ali <arbaaz.py@gmail.com>
2022-07-14 22:09:08 +0530

    Ordered linked list

---
+++ linked_list/ordered_linked_list.py
@@ -0,0 +1,62 @@
+from node import Node
+
+
+class OrderedList:
+    def __init__(self):
+        self.head = None
+
+    def search(self,item):
+        current = self.head
+        found = False
+        stop = False
+        while current != None and not found and not stop:
+            if current.get_data() == item:
+                found = True
+            elif current.get_data() > item:
+                stop = True
+            else:
+                current = current.get_next()
+        return found
+
+    def add(self,item):
+        current = self.head
+        previous = None
+        stop = False
+        while current != None and not stop:
+            if current.get_data() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.get_next()
+
+        temp = Node(item)
+        if previous is None:
+            temp.set_next(self.head)
+            self.head = temp
+        else:
+            temp.set_next(current)
+            previous.set_next(temp)
+
+    def is_empty(self):
+        return self.head is None
+
+    def size(self):
+        current = self.head
+        count = 0
+        while current != None:
+            count = count + 1
+            current = current.get_next()
+        return count
+
+
+mylist = OrderedList()
+mylist.add(31)
+mylist.add(77)
+mylist.add(17)
+mylist.add(93)
+mylist.add(26)
+mylist.add(54)
+
+print(mylist.size())
+print(mylist.search(93))
+print(mylist.search(100))
--- linked_list/unordered_linked_list.py
+++ linked_list/unordered_linked_list.py
@@ -1,23 +1,4 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-    def get_data(self):
-        return self.data
-
-    def get_next(self):
-        return self.next
-
-    def set_data(self, new_data):
-        self.data = new_data
-
-    def set_next(self, new_next):
-        self.next = new_next
-
-
-# temp = Node(93)
-# print(temp.get_data())
+from node import Node
 
 
 class UnorderedList:
